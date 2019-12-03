acb4160b9fcce7b6b2bdae927dc7046b1022f7de
CK <kainingc@usc.edu>
2019-12-03 11:45:41 -0800

    add quick select solutions

--- src/com/CK/Main.java
+++ src/com/CK/Main.java
@@ -1,6 +1,7 @@
 package com.CK;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Main {
 
@@ -13,10 +14,96 @@ public class Main {
     }
 }
 
-class Solution {
+class Solution3 {
     public int findKthLargest(int[] nums, int k) {
         int N = nums.length;
         Arrays.sort(nums);
         return nums[N - k];
+    }
+}
+
+class Solution2 {
+    public int findKthLargest(int[] nums, int k) {
+
+        final PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int val : nums) {
+            pq.offer(val);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int start = 0, end = nums.length - 1, index = nums.length - k;
+        while (start < end) {
+            int pivot = partion(nums, start, end);
+            if (pivot < index)
+                start = pivot + 1;
+            else if (pivot > index)
+                end = pivot - 1;
+            else
+                return nums[pivot];
+        }
+        return nums[start];
+    }
+
+    private int partion(int[] nums, int start, int end) {
+        int pivot = start;
+        while (start <= end) {
+            while (start <= end && nums[start] <= nums[pivot])
+                start++;
+            while (start <= end && nums[end] > nums[pivot])
+                end--;
+            if (start > end)
+                break;
+
+            swap(nums, start, end);
+        }
+        swap(nums, pivot, end);
+        return end;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int st = 0, ed = nums.length - 1, toFind = nums.length - k;
+        int index = partition(nums, st, ed);
+        while (index != toFind) {
+            if (index < toFind) {
+                index = partition(nums, index + 1, ed);
+            } else {
+                index = partition(nums, st, index - 1);
+            }
+        }
+        return nums[index];
+    }
+
+    private int partition(int[] nums, int st, int ed) {
+        int pivot = nums[ed], l = st;
+        for (int i = st; i < ed; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, i, l++);
+            }
+        }
+        swap(nums, l, ed);
+        return l;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
